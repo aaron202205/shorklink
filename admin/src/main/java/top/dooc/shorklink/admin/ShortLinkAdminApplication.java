7a78a7ba3160abbc3a31f7854c27cbf6cfdc084f
aaron <aaronfromstu@gmail.com>
2024-03-15 16:08:19 +0800

    【开发】【初始化项目】

---
+++ admin/src/main/java/top/dooc/shorklink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,15 @@
+package top.dooc.shorklink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author aaronchen
+ * @date 2024/3/15 下午3:51
+ */
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class,args);
+    }
+}
