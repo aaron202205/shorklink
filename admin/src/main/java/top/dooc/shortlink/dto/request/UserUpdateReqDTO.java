64f02106b0467725bbd687ec097a6d0f83108ff3
aaron <aaronfromstu@gmail.com>
2024-03-18 21:09:05 +0800

    【开发】【根据用户名修改用户信息】

---
+++ admin/src/main/java/top/dooc/shortlink/dto/request/UserUpdateReqDTO.java
@@ -0,0 +1,35 @@
+package top.dooc.shortlink.dto.request;
+
+import lombok.Data;
+
+/**
+ * @author aaronchen
+ * @date 2024/3/18 下午8:58
+ */
+@Data
+public class UserUpdateReqDTO {
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+}
